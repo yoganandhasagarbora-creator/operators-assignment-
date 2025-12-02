d87a9bb741267c51e7fe9222677905fe78059631
yoganandhasagarbora-creator <yoganandhasagarbora@gmail.com>
2025-12-02 09:33:47 +0530

    Update task.js

--- task.js
+++ task.js
@@ -1,37 +1,38 @@
-// var a=10;
-// var b="20";
+var a=10;
+var b="20";
 
-// console.log(a>b);
-// console.log(a<b);
-// console.log(a<=b);
-// console.log(a>=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
 
-// console.log(a == b);
-// console.log(a != b);
-// console.log(a === b);
-// console.log(a !== b);
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
 
 
-// var number = +prompt ("Enter A Number");
-// var data= (number%2 == 0)?"Even":"Odd";
-// console.log(data);
+var number = +prompt ("Enter A Number");
+var data= (number%2 == 0)?"Even":"Odd";
+console.log(data);
 
-// var age = +prompt("Enter age");
-// var data =(age >=18)?"Adult":"Minor";
-// console.log(data);
+var age = +prompt("Enter age");
+var data =(age >=18)?"Adult":"Minor";
+console.log(data);
 
-// var score = +prompt("Enter Score");
+var score = +prompt("Enter Score");
 
-// var grade = (score>=90)?"A":(score>=75)?"B":(score>=60)?"C":(score>=45)?"D":"F";
-// console.log(grade);
+var grade = (score>=90)?"A":(score>=75)?"B":(score>=60)?"C":(score>=45)?"D":"F";
+console.log(grade);
 
-// var climate = +prompt("Enter Today Temperature");
-// var tepmt=(climate>30)?"Hot":(climate>=20)?"Warm":(climate>=10)?"Cool":"Cold";
-// console.log(tepmt);
+var climate = +prompt("Enter Today Temperature");
+var tepmt=(climate>30)?"Hot":(climate>=20)?"Warm":(climate>=10)?"Cool":"Cold";
+console.log(tepmt);
 
 var age = +prompt("Enter Your Age");
 var data=(age<=12)?"Child":(age<=19)?"Teen":(age<=64)?"Adult":"Senior";
 console.log(data);
+
 
 
 
